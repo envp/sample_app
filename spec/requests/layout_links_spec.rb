34eab725e184dc6b8c3d5f38a96be8b6f76014e4
vagrant <vaibhav-y@users.noreply.github.com>
2014-06-27 18:11:12 +0000

    Added layout links tests and UsersController

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -6,22 +6,22 @@ describe "LayoutLinks", :type => :request do
     @base_title = ApplicationHelper::BASE_TITLE
   end
 
-  it "should have a Home page at '/'" do
+  it "has a Home page at '/'" do
     visit '/'
     expect(page).to have_title("#{@base_title} | Home")
   end
 
-  it "should have a Contact page at '/contact'" do
+  it "has a Contact page at '/contact'" do
     visit '/contact'
     expect(page).to have_title("#{@base_title} | Contact Us")
   end
 
-  it "should have an About page at '/about'" do
+  it "has an About page at '/about'" do
     visit '/about'
     expect(page).to have_title("#{@base_title} | About Us")
   end
 
-  it "should have a Help page at '/help'" do
+  it "has a Help page at '/help'" do
     visit '/help'
     expect(page).to have_title("#{@base_title} | Help")
   end
@@ -29,24 +29,50 @@ describe "LayoutLinks", :type => :request do
   describe "Logo link" do
 
     before :each do
-      @testcases = ['/', '/about', '/contact', '/help']
+      @testcases = [root_path, about_path, contact_path, help_path]
     end
 
-    it "should be a#logo element containing an img" do
+    it "has the logo image in an anchor to '/'" do
       visit @testcases.fetch(rand(@testcases.size))
-      expect(page).to have_css('a#logo img')
+      expect(page).to have_css("a[href='/'] > img")
     end
 
-    it "should redirect to '/' on clicking the logo" do
+    it "redirects to '/' on clicking the logo img" do
       visit '/'
       page_from_visit = page
 
       visit @testcases.fetch(rand(@testcases.size))
       click_link('logo')
-      expect(page).to have_title("#{@base_title} | Home")
       page_from_click = page
 
       expect(page_from_click).to eq(page_from_visit)
     end
+
+    it "has a signup page at /signup" do
+      visit '/signup'
+      expect(page).to have_title("Sign up")
+    end
+
+    it "has the right links on the layout" do
+      visit root_path
+      expect(page).to have_title('Home')
+
+      click_link 'About'
+      expect(page).to have_title('About')
+
+      click_link 'Contact'
+      expect(page).to have_title('Contact')
+
+      click_link 'Help'
+      expect(page).to have_title('Help')
+
+      click_link 'Home'
+      expect(page).to have_title('Home')
+
+      click_link 'Sign up now!'
+      expect(page).to have_title('Sign up')
+
+    end
+
   end
 end
