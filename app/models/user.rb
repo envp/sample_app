d75e7be516cc71fd76665bcbdda4005b480a03c3
vagrant <vaibhav-y@users.noreply.github.com>
2014-08-19 19:30:29 +0000

    Added secure password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 class User < ActiveRecord::Base
   # Strong parameters in the model take
   # care of whitelisting our mass asignment
@@ -18,5 +20,43 @@ class User < ActiveRecord::Base
     presence: true,
     confirmation: true,
     length: { within: 6..48 }
+
+
+  before_save :encrypt_password
+
+  def has_password?(submission)
+    # Store salt only once
+    self.encrypted_password == encrypt(submission)
+  end
+
+  class << self
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+
+      # No user exists in db
+      return nil if user.nil?
+
+      # Hit only if user has
+      return user if user.has_password?(submitted_password)
+    end
+  end
+  private
+
+  def encrypt_password
+    self.salt = make_salt if self.new_record?
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(data)
+    secure_hash("#{self.salt}--#{data}")
+  end
+
+  def secure_hash(data)
+    Digest::SHA2.hexdigest(data)
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{self.password}")
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,69 +8,70 @@ describe User, :type => :model do
       email: 'user@example.com',
       password: 'foobar',
       password_confirmation: 'foobar'
-
     }
   end
 
-  it "should create a new instance, given valid attributes" do
-    User.create!(@user_data)
-  end
+  describe "UserModel" do
+    it "should create a new instance, given valid attributes" do
+      User.create!(@user_data)
+    end
 
-  # Empty strings provide a more realistic
-  # simulation of the empty form situation
+    # Empty strings provide a more realistic
+    # simulation of the empty form situation
 
-  it "should require a name" do
-    anon_chan = User.new(@user_data.merge(name: ''))
-    expect(anon_chan).to_not be_valid
-  end
+    it "should require a name" do
+      anon_chan = User.new(@user_data.merge(name: ''))
+      expect(anon_chan).to_not be_valid
+    end
 
-  it "should require an email" do
-    anon_chan = User.new(@user_data.merge(email: ''))
-    expect(anon_chan).to_not be_valid
-  end
+    it "should require an email" do
+      anon_chan = User.new(@user_data.merge(email: ''))
+      expect(anon_chan).to_not be_valid
+    end
 
-  # Probably need to add 3 such validations for F/M*/L format of names
-  it "should reject names that are too long (larger than 48 chars)" do
-    verbose_name = 'a' * 49
-    verbose_name_user = User.new(@user_data.merge(name: verbose_name))
-    expect(verbose_name_user).to_not be_valid
-  end
+    # Probably need to add 3 such validations for F/M*/L format of names
+    it "should reject names that are too long (larger than 48 chars)" do
+      verbose_name = 'a' * 49
+      verbose_name_user = User.new(@user_data.merge(name: verbose_name))
+      expect(verbose_name_user).to_not be_valid
+    end
 
-  it "should accept valid email addresses" do
-    emails = %w[
-      user@example.org THE_DUDE@aBiD.eS
-      hello@hatana.ne.jp first.last@something.fishy.net
-    ]
+    it "should accept valid email addresses" do
+      emails = %w[
+        user@example.org THE_DUDE@aBiD.eS
+        hello@hatana.ne.jp first.last@something.fishy.net
+      ]
+        emails.each do |email|
+          valid_email_user = User.new(@user_data.merge(email: email))
+          expect(valid_email_user).to be_valid
+        end
+    end
+
+    it "should reject invalid email addresses" do
+      emails = %w[usr_at_goo.org so+you:think@you.can.dance
+        holy;shit\ its@vip.er troll\[s\]{b}(t)=r@ll.in
+      ]
+
       emails.each do |email|
-        valid_email_user = User.new(@user_data.merge(email: email))
-        expect(valid_email_user).to be_valid
+        invalid_email_user = User.new(@user_data.merge(email: email))
+        expect(invalid_email_user).to_not be_valid
       end
-  end
+    end
 
-  it "should reject invalid email addresses" do
-    emails = %w[usr_at_goo.org so+you:think@you.can.dance
-      holy;shit\ its@vip.er troll\[s\]{b}(t)=r@ll.in
-    ]
+    it "should reject duplicate email addresses" do
+      User.create!(@user_data)
+      dupe_usr = User.new(@user_data)
+      expect(dupe_usr).to_not be_valid
+    end
 
-    emails.each do |email|
-      invalid_email_user = User.new(@user_data.merge(email: email))
-      expect(invalid_email_user).to_not be_valid
+    it "should reject email addresses identical upto case" do
+      User.create!(@user_data)
+      upcase_usr = User.new(@user_data.merge(email: @user_data[:email].upcase))
+      expect(upcase_usr).to_not be_valid
     end
   end
 
-  it "should reject duplicate email addresses" do
-    User.create!(@user_data)
-    dupe_usr = User.new(@user_data)
-    expect(dupe_usr).to_not be_valid
-  end
-
-  it "should reject email addresses identical upto case" do
-    User.create!(@user_data)
-    upcase_usr = User.new(@user_data.merge(email: @user_data[:email].upcase))
-    expect(upcase_usr).to_not be_valid
-  end
-
-  describe "passwords" do
+  describe "Passwords" do
 
     before :each do
       @user = User.new(@user_data)
@@ -85,7 +86,7 @@ describe User, :type => :model do
     end
   end
 
-  describe "Password validations" do
+  describe "PasswordValidation" do
 
     it "should require a password field" do
       expect(User.new(
@@ -114,7 +115,7 @@ describe User, :type => :model do
     end
   end
 
-  describe "Encrypted password" do
+  describe "Encryption" do
     before :each do
       @user = User.create!(@user_data)
     end
@@ -122,8 +123,57 @@ describe User, :type => :model do
     it "should have an encrypted password attribute" do
       expect(@user).to respond_to(:encrypted_password)
     end
+
+    it "should set the encrypted_password attribute" do
+      expect(@user.encrypted_password).to_not be_blank
+    end
+
+    it "should have a exist" do
+      expect(@user).to respond_to(:salt)
+    end
+
+    describe "has_password? method" do
+      it "should exist" do
+        expect(@user).to respond_to(:has_password?)
+      end
+
+      it "should return true if passwords match" do
+        expect(@user).to have_password(@user_data[:password])
+      end
+
+      it "should return false if passwords don't match" do
+        expect(@user).to_not have_password("invalid")
+      end
+    end
   end
 
+  describe "Authentication" do
+
+    before :each do
+      @user = User.create!(@user_data)
+    end
+
+    it "should exist" do
+      expect(User).to respond_to(:authenticate)
+    end
+
+    # User types in a bad password
+    it "should return nil on email -> password mismatch" do
+      expect(User.authenticate(@user_data[:email], "totallynotthepassword")).to be_nil
+    end
+
+    # User forgets his/her email
+    # Feature: return an EmailNotFoundError
+    # to be caught and sent to /signup
+    it "should return nil for invalid email ids" do
+      expect(User.authenticate("does.not.exist@example.com", @user_data[:password])).to be_nil
+    end
+
+    it "should return the matched user with the given email on match" do
+      expect(User.authenticate(@user_data[:email], @user_data[:password])).to eq(@user)
+    end
+
+  end
 end
 
 
