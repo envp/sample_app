6df0408e3f2ba7187e2c165159b550d0cdfb9c2e
vagrant <vaibhav-y@users.noreply.github.com>
2014-08-21 20:02:31 +0000

    Added gravatar to user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def gravatar_image(user, opts = {size: 50})
+    gravatar_image_tag user.email.downcase,
+      :alt => user.name,
+      :class => 'gravatar',
+      :gravatar => opts
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -30,5 +30,22 @@ describe UsersController, :type => :controller do
       get :show, id: @user.id
       expect(assigns(:user)).to eq(@user)
     end
+
+    it "should have the right title" do
+      get :show, id: @user.id
+      expect(response.body).to have_title(@user.name)
+    end
+
+    it "should have the user's name" do
+      get :show, id: @user.id
+
+      # Keep the match as strict as possible
+      expect(response.body).to have_selector('h1', exact: @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, id: @user.id
+      expect(response.body).to have_selector('h1 > img.gravatar', match: :prefer_exact )
+    end
   end
 end
